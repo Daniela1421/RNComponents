8f23ce71c90e79d9e594199b5b5b6fd4b903cffa
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-12-19 12:28:11 -0500

    feat: initial structure

---
+++ src/components/FlatListMenuItem.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { MenuItem } from '../interface/appInterfaces';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+
+interface Props{
+  menuItem: MenuItem
+}
+
+export const FlatListMenuItem = ({menuItem}: Props) => {
+  const navigation = useNavigation()
+  
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigation.navigate(menuItem.component)}
+    >
+      <View style={styles.container}>
+        <Icon
+          name={menuItem.icon}
+          size={23}
+          color='grey'
+        />
+        <Text style= {styles.itemText}>{menuItem.name}</Text>
+        <View style={{flex: 1}}/>
+        <Icon
+          name='chevron-forward-outline'
+          size={23}
+          color='grey'
+        />
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flexDirection: 'row'
+  }, 
+  itemText: {
+    marginLeft: 10, 
+    fontSize: 19
+  }
+});
---
+++ src/screens/Animation101Screen.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native';
+
+export const Animation101Screen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.purpleBox}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  purpleBox: {
+    backgroundColor: '#5856d6', 
+    width: 150, 
+    height: 150
+  }
+});
